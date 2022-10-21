a35b5f94383c4d53443b5d0680895157bf261fd4
Ahmad Waliyudin <67423008+wilzzadex@users.noreply.github.com>
2022-10-21 11:06:14 +0700

    STRUKTUR DATA - BIG O NOTATION AHMAD WALIYUDDIN

---
+++ Contoh1.java
@@ -0,0 +1,8 @@
+public class Program
+{
+    public static void main(String[] args) {
+        // Ieu contoh nya
+        System.out.println("yedd");
+		
+	}
+}
---
+++ Contoh2.java
@@ -0,0 +1,9 @@
+public class Program
+{
+    public static void main(String[] args) {
+        // Ieu contoh 2
+        System.out.println("yedd");
+        System.out.println("yedd");
+		
+	}
+}
